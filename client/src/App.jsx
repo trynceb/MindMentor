37cc273f1411ca1ca8bbf63f47d54979341c1e4f
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-07 13:00:40 -0500

    create and connect chat page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Chat from "./components/chat"
+
 
 function App() {
 
   return (
     <div className="App">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
