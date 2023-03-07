1d981353fccebac0a2fe7ba7fb6b40c510044626
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-07 13:32:11 -0500

    create header and format structure

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced"
+import Header from "@/components/customHeader"
 
 const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "trynce",
+    "1234"
+  )
+
   return (
-    <div>Chat</div>
+    <div style ={{ flexBasis: "100%"}}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow 
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
   )
 }
 export default Chat
